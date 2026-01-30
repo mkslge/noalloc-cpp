ef160c1db474b657a9cff891317ae3770dffed57
mkslge <marklseeliger@gmail.com>
2026-01-30 08:03:00 -0500

    completed cmake

---
+++ build/CMakeFiles/basic.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic.dir/examples/basic.cpp.o"
+  "CMakeFiles/basic.dir/examples/basic.cpp.o.d"
+  "CMakeFiles/basic.dir/noalloc.cpp.o"
+  "CMakeFiles/basic.dir/noalloc.cpp.o.d"
+  "basic"
+  "basic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
